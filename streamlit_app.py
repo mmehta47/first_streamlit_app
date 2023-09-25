6f55fbf21023ead847093344dac55105fd49ad6c
mmehta47 <146020245+mmehta47@users.noreply.github.com>
2023-09-25 14:43:51 -0400

    Update streamlit_app.py
    fixed the read file statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & Blueberry Oatmeal")
 
 import pandas
-my_fruit_list = pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
